ca426c23ad691c7f14bd628af956997a5a2d6d95
Sashank030 <145583612+Sashank030@users.noreply.github.com>
2023-10-07 13:17:09 +0530

    knapsack.js

--- knapsack.js
+++ knapsack.js
@@ -1,5 +1,13 @@
 var items = [];
 var capacity = 0;
+document.getElementById("show_algorithm").addEventListener("click", function () {
+    var algorithmContent = document.getElementById("algorithm-content");
+    if (algorithmContent.style.display === "none") {
+        algorithmContent.style.display = "block";
+    } else {
+        algorithmContent.style.display = "none";
+    }
+});
 function addItem() {
     var itemName = document.querySelector(".item-name").value;
     var itemValue = parseInt(document.querySelector(".item-value").value);
@@ -18,7 +26,6 @@ function addItem() {
         alert("Please enter valid item details.");
     }
 }
-
 function clearItemInputs() {
     document.querySelector(".item-name").value = "";
     document.querySelector(".item-value").value = "";
@@ -31,7 +38,8 @@ function calculateKnapsack() {
         alert("Please add items and enter a valid knapsack capacity.");
         return;
     }
-    var selectedItems = knapsack(items, capacity);
+    var { selectedItems, dpTable } = knapsack(items, capacity);
+    displayDPTable(dpTable);
     var totalValue = 0;
 
     for (var i = 0; i < selectedItems.length; i++) {
@@ -45,24 +53,39 @@ function calculateKnapsack() {
         var item = selectedItems[i];
         knapsackOutput.innerHTML += `<p>${item.name} (Value: ${item.value}, Weight: ${item.weight})</p>`;
     }
-
-    knapsackOutput.innerHTML += `<p>Total profit: ${totalValue}</p>`;
+    knapsackOutput.innerHTML += `<p>Total Value: ${totalValue}</p>`;
     document.getElementById("capacity").value = capacity;
 }
-
 document.getElementById("add_item").addEventListener("click", addItem);
 document.getElementById("calculate_knapsack").addEventListener("click", calculateKnapsack);
-
+function displayDPTable(dpTable) {
+    var dpTableOutput = document.getElementById("dp-table");
+    dpTableOutput.innerHTML = "<h2>DP Table (Step by Step):</h2>";
+    for (var step = 0; step < dpTable.length; step++) {
+        dpTableOutput.innerHTML += `<h3>Step ${step + 1}:</h3>`;
+        dpTableOutput.innerHTML += "<table border='1'>";
+        for (var i = 0; i < dpTable[step].length; i++) {
+            dpTableOutput.innerHTML += "<tr>";
+            for (var j = 0; j < dpTable[step][i].length; j++) {
+                dpTableOutput.innerHTML += `<td>${dpTable[step][i][j]}</td>`;
+            }
+            dpTableOutput.innerHTML += "</tr>";
+        }
+        dpTableOutput.innerHTML += "</table>";
+    }
+}
 function knapsack(items, capacity) {
     var n = items.length;
-    var dp = new Array(n + 1).fill(0).map(() => new Array(capacity + 1).fill(0));
-
+    var dpTable = [];
+    for (var step = 0; step <= n; step++) {
+        dpTable.push(new Array(capacity + 1).fill(0));
+    }
     for (var i = 1; i <= n; i++) {
         for (var w = 1; w <= capacity; w++) {
             if (items[i - 1].weight <= w) {
-                dp[i][w] = Math.max(dp[i - 1][w], dp[i - 1][w - items[i - 1].weight] + items[i - 1].value);
+                dpTable[i][w] = Math.max(dpTable[i - 1][w], dpTable[i - 1][w - items[i - 1].weight] + items[i - 1].value);
             } else {
-                dp[i][w] = dp[i - 1][w];
+                dpTable[i][w] = dpTable[i - 1][w];
             }
         }
     }
@@ -70,14 +93,13 @@ function knapsack(items, capacity) {
     var selectedItems = [];
     var i = n;
     var w = capacity;
-
     while (i > 0 && w > 0) {
-        if (dp[i][w] !== dp[i - 1][w]) {
+        if (dpTable[i][w] !== dpTable[i - 1][w]) {
             selectedItems.push(items[i - 1]);
             w -= items[i - 1].weight;
         }
         i--;
     }
 
-    return selectedItems;
+    return { selectedItems, dpTable };
 }
